ec94bb6e5ae318736b473d04b1ea5519d8726e6d
aduranterres <antonio@joomdle.com>
2018-12-19 12:54:30 +0100

    Add files via upload

--- db/install.php
+++ db/install.php
@@ -15,41 +15,17 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Capabilities for signup enrolment plugin.
+ * Signup enrolment plugin installation.
  *
  * @package    enrol
  * @subpackage signup
+ * @author     Antonio Duran Terres
+ * @copyright  2011 Antonio Duran Terres
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$capabilities = array(
-
-    'enrol/signup:config' => array(
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_COURSE,
-        'archetypes' => array(
-            'manager' => CAP_ALLOW,
-        )
-    ),
-
-    'enrol/signup:manage' => array(
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_COURSE,
-        'archetypes' => array(
-            'manager' => CAP_ALLOW,
-            'editingteacher' => CAP_ALLOW,
-        )
-    ),
-
-    'enrol/signup:unenrol' => array(
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_COURSE,
-        'archetypes' => array(
-            'manager' => CAP_ALLOW,
-        )
-    ),
-
-);
-
+function xmldb_enrol_signup_install() {
+    global $CFG, $DB;
+}
